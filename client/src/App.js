596d3efa0521bf4e7f6bf8b2387b95b287e34ca9
Prerit Khandelwal <prerit.web@gmail.com>
2024-05-29 18:17:15 +0530

    removed extra code

--- client/src/App.js
+++ client/src/App.js
@@ -16,18 +16,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/my-list" element={<MyList />} />
-
-
-          {/* <Route path="/product-details/:id" element={<ProductDetails />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout-details" element={<CheckoutDetails />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/checkout-success" element={<CheckoutSuccess />} />
-          <Route path="/order-history" element={<OrderHistory />} />
-          <Route path="/order-details/:id" element={<OrderDetails />} />
-          <Route path="/review-product/:id" element={<ReviewProducts />} />
-          <Route path="/admin/*" element={<Admin />} />
-          <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
